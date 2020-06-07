1c19a32c0fe865b6ebf0fe93b0edf49a7633028e
Sohail Khn <sohailkhan.sk@hotmail.com>
2020-06-07 23:19:17 +0500

    headers set

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE,UPDATE');
-    res.setHeader('Acess-Control-Allow-Headers','Content-Type','Authorization');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
     next();
 })
 
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user');
-
 const authMiddleware = require('../middlewares/auth');
-
 
 router.post('/register',userController.signUp);
 router.post('/login',userController.signIn);
